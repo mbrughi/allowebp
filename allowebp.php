cf60c02e3bf4b55494471d6cac1ebcf62a635419
Marco Brughi <mbrughi@users.noreply.github.com>
2021-01-07 11:40:59 +0100

    Create allowebp.php
    first commit

---
+++ allowebp.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name: ALLOWEBP
+ * Plugin URI:  https://github.com/mbrughi/allowebp/
+ * Description: Allow Wordpress to upload .webp media files format.
+ * Version:     1.1.0
+ * Author:      Marco Brughi
+ * Author URI:  https://marcobrughi.com
+ * License:     GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
